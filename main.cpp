2ca6cdb71c3d195ff5eafe3876d42ef7e326c312
Deyan Dobromirov <dvd_video@abv.bg>
2015-03-10 16:27:58 +0200

    LoadCPUAHundread: Initial commit

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <time.h>
+#include <process.h>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+#define ARR_LEN 100000000
+
+int iArr[ARR_LEN];
+
+void gotoXY(short int x,short int y)
+{
+   SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), (COORD){x,y} );
+}
+
+void Thread( void* pParams )
+{
+  int i, num = 0;
+
+  while ( 1 )
+  {
+    for (i = 0; i < ARR_LEN; i++)
+    {
+      iArr[i] = (i*(--num))-i;
+    }
+  }
+}
+
+int main( void )
+{
+  int i = 0, iThr;
+
+  printf("Desired threads count: ");
+  scanf("%d",&iThr);
+  for(i=0;i<iThr;i++)
+  {
+      _beginthread( Thread, 1000, NULL );
+  }
+
+  printf("Threads allocated !\n");
+  i = 0;
+  while( 1 )
+  {
+    gotoXY(0,2);
+    printf("Iteration: %d            \n",i++);
+  }
+
+  return 0;
+}
